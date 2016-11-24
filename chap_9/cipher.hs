6cc1577f34ac8de452e62503dd6212a49d135196
Christopher Lee <cristovao.m.lee@gmail.com>
2016-11-24 10:56:40 -0500

    caeser cipher working

--- chap_9/cipher.hs
+++ chap_9/cipher.hs
@@ -1,11 +1,15 @@
 module Cipher where
   import Data.Char
 
-  shiftChar:: Char -> Int -> Char
-  shiftChar x shift =
+  shiftChar:: Int -> Char -> Char
+  shiftChar shift x =
     case ((+) shift $ ord x) > 122 of
       True -> chr . (+) 96 $ flip mod 122 $ ord x + shift
       False -> chr . (+) shift $ ord x
+
+  main :: IO ()
+  main =
+    print $ map (shiftChar 1) "hello"
 
   -- either mod 122 or 97
 
