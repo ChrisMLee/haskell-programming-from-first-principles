4f37f96cb2679652d1e49263c88da2180f856bad
Christopher Lee <cristovao.m.lee@gmail.com>
2017-02-21 09:17:01 -0500

    final chap_18 exercises

--- chap_18/chapter_exercises.hs
+++ chap_18/chapter_exercises.hs
@@ -4,6 +4,7 @@ import Control.Applicative
 import Test.QuickCheck
 import Test.QuickCheck.Checkers
 import Test.QuickCheck.Classes
+import Control.Monad
 
 
 -- 1.
@@ -58,18 +59,121 @@ instance (Arbitrary a, Arbitrary b) => Arbitrary (PhhhbbtttEither b a) where
 
 instance (Eq a, Eq b) => EqProp (PhhhbbtttEither a b) where (=-=) = eq
 
+--3. 
+
+newtype Identity a = 
+  Identity a
+  deriving (Eq, Ord, Show)
+
+instance Functor Identity where
+  fmap f (Identity a) = Identity (f a)
+
+instance Applicative Identity where
+  pure = Identity
+  (Identity f) <*> (Identity a) = Identity (f a) 
+
+instance Monad Identity where
+  return = pure
+  (>>=) (Identity a) f = f a
+
+instance (Eq a) => EqProp (Identity a) where (=-=) = eq
+
+instance Arbitrary a => Arbitrary (Identity a) where
+  arbitrary = do
+  a <- arbitrary
+  return $ (Identity a)
+
+--4. 
+
+data List a =
+    Nil
+  | Cons a (List a)
+  deriving (Eq, Show)
+
+
+instance Functor List where
+  fmap _ Nil = Nil
+  fmap f (Cons a x) = Cons (f a) (fmap f x)
+
+instance Applicative List where
+  pure x = Cons x Nil
+  Cons f y <*> Cons x z = flatMap (\a -> fmap a (Cons x z)) (Cons f y)
+  _ <*> Nil = Nil
+  Nil <*> _ = Nil
+
+append :: List a -> List a -> List a
+append Nil ys = ys
+append (Cons x xs) ys = Cons x $ xs `append` ys
+
+fold :: (a -> b -> b) -> b -> List a -> b
+fold _ b Nil = b
+fold f b (Cons h t) = f h (fold f b t)
+
+concat' :: List (List a) -> List a
+concat' = fold append Nil
+
+-- write this one in terms of concat' and fmap
+flatMap :: (a -> List b) -> List a -> List b
+flatMap f as = concat' (fmap f as)
+
+instance Monad List where
+  return = pure
+  (>>=) (Nil) f = Nil
+  (>>=) (Cons x y) f = (f x) `append` (y >>= f)
+  -- y :: List a
+  -- f x :: List b
+  -- y  :: List b
+  -- (a -> List b)
+
+instance (Arbitrary a) => Arbitrary (List a) where
+  arbitrary = list2List <$> suchThat arbitrary (\x -> length x < 20)
+
+list2List :: [a] -> List a
+list2List [] = Nil
+list2List (x:xs) = Cons x (list2List xs)
+
+instance (Eq a) => EqProp (List a) where (=-=) = eq
+
 main :: IO()
 main = do
   quickBatch $ applicative (undefined :: Nope (Int, Double, Char))
   quickBatch $ monad (undefined :: Nope (Int, Double, Char))
   quickBatch $ applicative (undefined :: PhhhbbtttEither String (Int, Double, Char))
   quickBatch $ monad (undefined :: PhhhbbtttEither String (Int, Double, Char))
+  quickBatch $ applicative (undefined :: Identity (Int, Double, Char))
+  quickBatch $ monad (undefined :: Identity (Int, Double, Char))
+  quickBatch $ applicative (undefined :: List (Int, Double, Char))
+  quickBatch $ monad (undefined :: List (Int, Double, Char))
 
 
 
+-- Write the following functions using the methods provided by Monad and Functor.
+-- Using stuff like identity and composition is fine, but it has to typecheck with types provided.
 
+--1.
+j :: Monad m => m (m a) -> m a
+j = join
 
+--2.
+l1 :: Monad m => (a -> b) -> m a -> m b
+l1 = liftM
 
+--3.
+l2 :: Monad m => (a -> b -> c) -> m a -> m b -> m c
+l2 = liftM2
+
+--4. 
+a :: Monad m => m a -> m (a -> b) -> m b
+a = flip ap
+
+--5.
+meh :: Monad m => [a] -> (a -> m b) -> m [b]
+meh [] _ = pure []
+meh (x:xs) f =  (:) <$> f x <*> (meh xs f)
+
+-- meh = forM 
+
+--6. 
 
 
 
--- chap_18/exercises.md
+++ chap_18/exercises.md
@@ -9,6 +9,13 @@ The argument the monad TestBatch wants is identical to the Applicative, a tuple 
 
 Monad is an applicative that can bind
 
+Applicative makes no guarantee of order.  
+Things are guaranteed to happen in order in a Monad.  
+
+You should only use Monad if you have to - ytry to use the weakest possible constraint you can.
+Otherwise you pigeonhole yourself into doing things sequential
+
+IO, errors, data structures
 
 ### The answer is the exercise
 ```
