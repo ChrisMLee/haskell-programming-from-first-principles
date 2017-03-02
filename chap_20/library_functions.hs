501ad9f24d3b82cd81a473d5cb0f070e9ce9d1b6
Christopher Lee <christopherlee@ip-10-10-20-177.ec2.internal>
2017-03-02 18:46:07 -0500

    work progress on exercises

--- chap_20/library_functions.hs
+++ chap_20/library_functions.hs
@@ -1,8 +1,26 @@
 module LibaryFunctions where
 
 import Data.Monoid
+import Data.Maybe
 
 --1. 
 
 sum' :: (Foldable t, Num a) => t a -> a
-sum' = getSum $ foldMap Sum
+sum' = getSum . foldMap Sum
+
+--2. 
+
+product' :: (Foldable t, Num a) => t a -> a
+product' = getProduct . foldMap Product
+
+--3. 
+
+elem' :: (Foldable t, Eq a) => a -> t a -> Bool
+elem' a ta = foldr (\x acc -> if x == a then True else False) False ta
+
+--4.
+minimum' :: (Foldable t, Ord a) => t a -> Maybe a
+minimum' ta = foldr z Nothing ta
+             where
+              z x Nothing = Just x
+              z x (Just y) = if x < y then (Just x) else (Just y)
