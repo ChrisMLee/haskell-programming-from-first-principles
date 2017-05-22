e91fc001501870ca26e0e33a1034211eb68edbef
Christopher Lee <cristovao.m.lee@gmail.com>
2017-05-22 08:48:44 -0400

    finished averaging func

--- chap_24/log_activity.hs
+++ chap_24/log_activity.hs
@@ -8,18 +8,10 @@ import Text.Trifecta
 import Data.List
 import Control.Applicative
 import Data.Time
-import Data.Monoid
+import Data.Semigroup (Semigroup, (<>))
+import Data.Char
 
 -- http://ekmett.github.io/trifecta/
-
--- TotalLog data type made up of different activities
--- parseActivity: monadic action that grabs the name of the activty, its time
--- and the time that it took until the next activity
--- use mod for minutes out of hour
--- tuple (8, 45, title)
-
--- skip comments
--- skip date
 
 newtype Hours a = Hours a deriving (Eq, Show, Functor)
 newtype Minutes a = Minutes a deriving (Eq, Show, Functor)
@@ -28,6 +20,22 @@ newtype Title a = Title a deriving (Eq, Show, Functor)
 data Activity a =
   Activity (Hours a) (Minutes a) (Title a) 
   deriving (Eq, Show, Functor)
+
+getActivityTime :: (Activity String) -> String 
+getActivityTime (Activity h m t) = getActivityTime h m where
+                                    getActivityTime (Hours x) (Minutes y) = x <> ":" <> y
+
+getActivityTitle :: (Activity String) -> String
+getActivityTitle (Activity h m t) = getActivityTitle t where
+                                      getActivityTitle (Title x) = x
+
+getActivityHours :: (Activity String) -> String
+getActivityHours (Activity h m t) = getActivityHours h where
+                                      getActivityHours (Hours x) = x
+
+getActivityMinutes :: (Activity String) -> String
+getActivityMinutes (Activity h m t) = getActivityMinutes m where
+                                      getActivityMinutes (Minutes x) = x
 
 newtype Comment = Comment String deriving (Eq, Show)
 -- Todo: change to a type with a month, date, year
@@ -56,7 +64,7 @@ parseActivity = do
   h <- count 2 digit
   skipMany (oneOf ":")
   m <- count 2 digit
-  t <- space *> manyTill anyChar (try (string "\n") <|> (string "--" *> space *> manyTill anyChar (try (string "\n"))))
+  t <- space *> manyTill anyChar (try (string "\n") <|> (string " --" *> space *> manyTill anyChar (try (string "\n"))))
   return (Activity (Hours h) (Minutes m) (Title t))
 
 parseComment :: Parser Comment
@@ -64,10 +72,6 @@ parseComment = Comment <$> (string "--" *> space *> manyTill anyChar (try (strin
 
 parseDate :: Parser Date
 parseDate = Date <$> (char '#' *> space *> manyTill anyChar (try (string "\n")))
-
--- letterDashSpace :: Parser Char
--- letterDashSpace = try letter <|> char '-' <|> char ' ' <|> char '&'
--- try parseActivity, try parseComment
 
 parseWord :: Parser [[Char]]
 parseWord = try (some (token $ some letter)) <|> return []
@@ -91,11 +95,10 @@ skipEOL = skipMany (oneOf "\n")
 
 -- parseSection :: Parser Line
 parseSection = do
+  skipMany parseComment
   date <- LineDate <$> parseDate
   lines <- some parseLine
   return (date : lines)
-
-
 
 -- via http://stackoverflow.com/questions/376968/convert-haskell-int-with-leading-zero-to-string/376982#376982
 show2d :: Int -> String 
@@ -107,6 +110,41 @@ timeFormat :: String
 timeFormat = "%H:%M"
 understandTime = parseTimeOrError True defaultTimeLocale timeFormat
 
+-- http://stackoverflow.com/questions/25806378/how-do-i-convert-difftime-to-nominaldifftime
+
+formatMinutes :: UTCTime -> String
+formatMinutes = formatTime defaultTimeLocale "%M"
+
+formatHours :: UTCTime -> String
+formatHours = formatTime defaultTimeLocale "%H"
+
+-- http://stackoverflow.com/questions/14656762/no-instance-for-fractional-int-arising-from-a-use-of
+-- divideActivityTime :: Integer -> (Activity String) -> (Activity String)
+averageActivityTime n a = 
+   Activity (Hours h') (Minutes m') (Title t) where
+    t = getActivityTitle a
+    h = getActivityHours a
+    m = getActivityMinutes a
+    timeInSeconds = ((base10Conv $ map digitToInt h) * 60 * 60) + ((base10Conv $ map digitToInt m) * 60)
+    averageTime   = timeInSeconds `div` n
+    h' = show2d $ averageTime `div` 60 `div` 60
+    m' = show2d $ averageTime `div` 60 `rem` 60
+
+-- Activity (Hours h') (Minutes m') (Title t) 
+
+combineActivities :: (Activity String) -> (Activity String) -> (Activity String)
+combineActivities a a' =
+  Activity (Hours h'') (Minutes m'') (Title t) where
+    t = getActivityTitle a
+    h = getActivityHours a
+    m = getActivityMinutes a
+    h' = getActivityHours a'
+    m' = getActivityMinutes a'
+    additionalSeconds = ((base10Conv $ map digitToInt h') * 60 * 60) + ((base10Conv $ map digitToInt m') * 60)
+    sumTime = addUTCTime ((fromRational . toRational) additionalSeconds) (understandTime (h <> ":" <> m))
+    h'' = formatHours sumTime
+    m'' = formatMinutes sumTime
+
 -- via http://stackoverflow.com/questions/31087004/print-nominaldifftime-as-hours-minutes-and-seconds
 calculateActivityTime :: (Activity String) -> (Activity String) -> (Activity String)
 calculateActivityTime (Activity h m t) (Activity h' m' t') = 
@@ -114,8 +152,8 @@ calculateActivityTime (Activity h m t) (Activity h' m' t') =
     activityWithDuration (Hours x) (Minutes y) (Hours x') (Minutes y') (Title z) =
       Activity (Hours h'') (Minutes m'') (Title z) where
         diff = diffUTCTime (understandTime (x <> ":" <> y)) (understandTime (x' <> ":" <> y'))
-        h'' = show2d $ abs $ floor (diff)  `div` 60 `div` 60
-        m'' = show2d $ abs $ floor (diff)  `div` 60 `rem` 60
+        h'' = show2d $ (if abs diff < 3600 then 0 else abs $ floor (diff) `div` 60 `div` 60)
+        m'' = show2d $ (if abs diff < 60 then 0  else abs $ floor (diff) `div` 60 `rem` 60)
 
 calculateActivityTimes :: [Line] -> [(Activity String)]
 calculateActivityTimes list = foldr timeTillNext [] list where
@@ -128,12 +166,24 @@ calculateActivityTimes list = foldr timeTillNext [] list where
                                     next Nothing  = midnight
                                 timeTillNext (LineComment x) b  = b
                                 timeTillNext (LineDate x) b     = b
--- diffUTCTime
--- DiffTime
--- floor (diffUTCTime timeDos timeUno)
--- floor (diffUTCTime timeDos timeUno) `div` 60 `rem` 60
--- weirdThing = foldr cool [] [1,2,3] 
---               where cool a b = (a + 1) : b
+
+sumActivityTimes :: [(Activity String)] -> [(Activity String)]
+sumActivityTimes activityList = foldr sumFunc [] activityList where
+                                  sumFunc a b = if hasTitle then 
+                                      combineActivities a (b !! (getIndex matchIndex)) : filteredList else
+                                      a : b where 
+                                    title             = getActivityTitle a
+                                    currentTitles     = getActivityTitle <$> b
+                                    hasTitle          = elem title currentTitles
+                                    matchIndex        = elemIndex title currentTitles
+                                    filteredList      = filter (\x -> getActivityTitle x /= title) b
+                                    getIndex (Just x) = x
+
+-- total activity times / number of dates
+getDates :: [Line] -> [Date]
+getDates activityList = foldr getDate [] activityList where
+                          getDate (LineDate x) b = x : b
+                          getDate _ b  = b
 
 sectionOne :: String
 sectionOne = [r|
@@ -220,36 +270,11 @@ logFile = [r|
 22:00 Sleep
 |]
 
--- [08:00, 09:00, 11:00, 12:00, 13:00, 17:00, 17:30, 19:00, 21:00, 21:15, 22:00]
-
--- list, fold list + position 
--- Calculate activity times
---  
-
 main :: IO()
 main = do
-  -- print $ parseString parseActivity mempty "08:00 Breakfast"
-  -- print $ parseString parseActivity mempty "09:00 Sanitizing moisture collector"
-  -- print $ parseString (some parseLine) mempty sectionOne 
-  -- print $ parseString parseLogFile mempty logFile
-  -- print $ calculateActivityTimes <$> (parseString parseLogFile mempty logFile)
-  -- print $ printActivity <$$>  (calculateActivityTimes <$> (parseString parseLogFile mempty logFile))
-  print $ parseString parseLogFile mempty sectionOne
+  print $ "Total time spent:"
+  print $ printActivity <$$> sumActivityTimes <$> Data.List.concat <$> (fmap . fmap) (calculateActivityTimes) (parseString parseLogFile mempty logFile)
 
-
--- fmap calculateActivityTimes 
-
--- reduce
--- LineActivity x
--- b 
-
-
-
--- [LogSection, LogSection]
--- LineActivity (Activity (Hours "19") (Minutes "00") (Title "Dinner"))
--- LineActivity (Activity (Hours "21") (Minutes "00") (Title "Shower"))
--- Activity1 (function) Activity2 = [Title, Duration], [Title, Hours]
-
-
-
+  print $ "Average time spent per day:" 
+  print $  printActivity <$$> ( (fmap <$> (averageActivityTime <$> (length <$> getDates <$> Data.List.concat <$> (parseString parseLogFile mempty logFile)))) <*> (sumActivityTimes <$> Data.List.concat <$> (fmap . fmap) (calculateActivityTimes) (parseString parseLogFile mempty logFile)) )
 
